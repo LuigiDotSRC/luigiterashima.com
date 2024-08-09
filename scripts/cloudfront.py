d7da18cc1ccdf888c77ee729c5eb0899725fe4b9
LuigiDotSRC <luigi.terashima@gmail.com>
2024-08-09 16:42:52 -0400

    create cloudfront invalidation script and add to push_changes.py

---
+++ scripts/cloudfront.py
@@ -0,0 +1,42 @@
+from botocore.exceptions import ClientError
+from dotenv import load_dotenv
+import boto3
+import time
+import os 
+
+load_dotenv()
+
+DISTRIBUTION_ID = os.getenv('DISTRIBUTION_ID')
+
+def create_invalidation(paths):
+    """Create an invalidation for a CloudFront distribution.
+
+    :param paths: List of paths to invalidate (e.g., ['/*'])
+    :return: Invalidation ID if successful, None otherwise
+    """
+    cloudfront_client = boto3.client('cloudfront')
+    
+    try:
+        # Create the invalidation batch with unique caller reference
+        caller_reference = str(time.time()).replace('.', '')
+        invalidation_batch = {
+            'Paths': {
+                'Quantity': len(paths),
+                'Items': paths
+            },
+            'CallerReference': caller_reference
+        }
+
+        # Create the invalidation
+        response = cloudfront_client.create_invalidation(
+            DISTRIBUTION_ID,
+            InvalidationBatch=invalidation_batch
+        )
+
+        invalidation_id = response['Invalidation']['Id']
+        print(f'Invalidation created with ID: {invalidation_id}')
+        return invalidation_id
+
+    except ClientError as e:
+        print(f'Error creating invalidation: {e}')
+        return None
--- scripts/push_changes.py
+++ scripts/push_changes.py
@@ -1,5 +1,6 @@
 from s3 import upload_file, get_bucket_timestamps
 from file_check import get_file_modification_time, get_files_in_dir
+from cloudfront import create_invalidation
 from dotenv import load_dotenv
 from datetime import timedelta
 import os 
@@ -10,6 +11,7 @@ FILE_DIR = os.getenv('FILE_DIR')
 BUCKET_NAME = os.getenv('BUCKET_NAME')
 
 if __name__ == '__main__':
+    updated_files = []
     files = get_files_in_dir(FILE_DIR)  
     bucket_timestamps = get_bucket_timestamps(BUCKET_NAME)
 
@@ -23,3 +25,8 @@ if __name__ == '__main__':
             except KeyError:
                 print(f'{file_name} not in s3')
             upload_file(file, BUCKET_NAME, file_name)
+            updated_files.append(f'/{file_name}')
+    
+    if updated_files:
+        invalidation = create_invalidation(updated_files)     
+        print(f'started invalidation | {invalidation}')
